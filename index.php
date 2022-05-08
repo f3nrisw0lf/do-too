56b28a911c10e6fdb012f1ab880feb8ef8b912ae
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-05-08 16:08:11 +0800

    Added Semantic UI Dependency

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/semantic.min.css">
+  <link rel="stylesheet" href="./css/semantic.css">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <button>Hello World</button>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="./css/semantic.min.js"></script>
+</body>
+
+</html>
