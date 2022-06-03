f1676a4d58ebbd94b4ebf7e3173cfb0c4b64ab76
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-06-04 00:06:14 +0800

    Improved Landing Page UI: Added SVG and Better Layout

--- src/app.php
+++ src/app.php
@@ -1,5 +1,5 @@
 <?php
 include_once("db_connection.php");
 // include_once("components/Nav.php");
-include_once("pages/Me.php");
-// include_once("pages/Landing.php");
+// include_once("pages/Me.php");
+include_once("pages/Landing.php");
--- src/components/Nav.php
+++ src/components/Nav.php
@@ -1,42 +1,26 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg bg-transparent navbar-light bg-light mb-auto">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"><strong>DoToo</strong></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarColor03">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-          </div>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-      <button class="btn btn-primary m-1 px-3 d-grid gap-2" type="submit">Login</button>
+    <div class="collapse navbar-collapse justify-content-between align-items-center" id="navbarColor03">
+      <div>
+      </div>
+      <div>
+        <ul class="navbar-nav me-auto gap-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">FAQs</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-outline-primary" aria-current="page" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </nav>
